4a5318a63030fc2f34aa249c2b636222e33de819
elfgod <albertogx@icloud.com>
2021-10-24 18:44:43 -0500

    Refactor /login and /logout Routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,29 @@
 const User = require('../models/user');
+const passport = require('passport');
 
 module.exports = {
+    // POST /register
     async postRegister(req, res, next) {
-        console.log('Registering user');
-        const newUser = new User({
-            username: req.body.username,
-            email: req.body.email,
-            image: req.body.image
-        });
-        
-        await User.register(newUser, req.body.password);
+      console.log('Registering user');
+      const newUser = new User({
+          username: req.body.username,
+          email: req.body.email,
+          image: req.body.image
+      });
+      
+      await User.register(newUser, req.body.password);
+      res.redirect('/');
+    },
+    // POST /login
+    postLogin(req, res, next) {
+      passport.authenticate('local', {
+          successRedirect: '/',
+          failureRedirect: '/login'
+        })(req, res, next);
+    },
+    // GET /logout
+    getLogout(req, res, next) {
+         req.logout();
         res.redirect('/');
-    }
+      }
 }
